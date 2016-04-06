da2d6d095d47935ac8be27603037895e500ad13a
iii-ishida <dev.j.ishida@gmail.com>
2016-04-06 11:17:29 +0900

    WorkCollection の Ordered を実装

--- workrec/work/work_collection.go
+++ workrec/work/work_collection.go
@@ -1,7 +1,53 @@
 package work
 
+import (
+	"sort"
+)
+
 type WorkCollection []Work
 
 func (workCollection WorkCollection) Ordered() WorkCollection {
-	return workCollection
+	ordered := make([]Work, len(workCollection))
+	copy(ordered, workCollection)
+
+	byStartTime().Sort(ordered)
+	return ordered
+}
+
+func byStartTime() by {
+	return func(work1, work2 *Work) bool {
+		startTime1 := work1.StartTime()
+		startTime2 := work2.StartTime()
+		if startTime1.Equal(startTime2) {
+			return true
+		}
+		return startTime1.After(startTime2)
+	}
+}
+
+type by func(work1, work2 *Work) bool
+
+func (by by) Sort(works []Work) {
+	sorter := &workSroter{
+		works: works,
+		by:    by,
+	}
+	sort.Sort(sorter)
+}
+
+type workSroter struct {
+	works []Work
+	by    by
+}
+
+func (s *workSroter) Len() int {
+	return len(s.works)
+}
+
+func (s *workSroter) Swap(i, j int) {
+	s.works[i], s.works[j] = s.works[j], s.works[i]
+}
+
+func (s *workSroter) Less(i, j int) bool {
+	return s.by(&s.works[i], &s.works[j])
 }
--- workrec/work/work_collection_test.go
+++ workrec/work/work_collection_test.go
@@ -2,8 +2,89 @@ package work
 
 import (
 	"testing"
+	"time"
 )
 
 func TestWorkCollectionOrder(t *testing.T) {
+	wants := []string{
+		"作業03",
+		"作業01",
+		"作業05",
+		"作業02",
+		"作業04",
+	}
 
+	original := testWorkCollection
+
+	ordered := original.Ordered()
+	for i, work := range ordered {
+		want := wants[i]
+		if work.Title != want {
+			t.Errorf("ordered[%d] = %+v, want %+v", i, work, want)
+		}
+	}
+
+	wants = []string{
+		"作業01",
+		"作業02",
+		"作業03",
+		"作業04",
+		"作業05",
+	}
+	for i, work := range original {
+		want := wants[i]
+		if work.Title != want {
+			t.Errorf("original[%d] = %+v, want %+v", i, work, want)
+		}
+	}
+}
+
+var testWorkCollection = WorkCollection{
+	func() Work {
+		work := New("作業01", 0)
+		work = work.Start(time.Date(2015, 7, 29, 9, 30, 0, 0, time.UTC))
+		work = work.Toggle(time.Date(2015, 7, 29, 12, 30, 0, 0, time.UTC))
+		work = work.Toggle(time.Date(2015, 7, 29, 13, 30, 0, 0, time.UTC))
+		work = work.Toggle(time.Date(2015, 7, 29, 18, 30, 0, 0, time.UTC))
+		return work
+	}(),
+	func() Work {
+		work := New("作業02", 0)
+		work = work.Start(time.Date(2015, 7, 28, 10, 00, 0, 0, time.UTC))  // 開始
+		work = work.Toggle(time.Date(2015, 7, 28, 12, 00, 0, 0, time.UTC)) // 停止
+		work = work.Toggle(time.Date(2015, 7, 28, 14, 30, 0, 0, time.UTC)) // 再開
+		work = work.Toggle(time.Date(2015, 7, 28, 19, 30, 0, 0, time.UTC)) // 停止
+		work = work.Toggle(time.Date(2015, 7, 29, 10, 00, 0, 0, time.UTC)) // 再開
+		work = work.Toggle(time.Date(2015, 7, 29, 12, 00, 0, 0, time.UTC)) // 停止
+		work = work.Toggle(time.Date(2015, 7, 29, 14, 30, 0, 0, time.UTC)) // 再開
+		work = work.Toggle(time.Date(2015, 7, 30, 1, 00, 0, 0, time.UTC))  // 停止
+		work = work.Toggle(time.Date(2015, 7, 30, 13, 30, 0, 0, time.UTC)) // 再開
+		work = work.Toggle(time.Date(2015, 7, 30, 15, 30, 0, 0, time.UTC)) // 停止
+		work = work.Toggle(time.Date(2015, 7, 30, 16, 30, 0, 0, time.UTC)) // 再開
+		return work
+	}(),
+	func() Work {
+		work := New("作業03", 0)
+		work = work.Start(time.Date(2015, 7, 30, 10, 00, 0, 0, time.UTC))  // 開始
+		work = work.Toggle(time.Date(2015, 7, 30, 12, 00, 0, 0, time.UTC)) // 停止
+		work = work.Toggle(time.Date(2015, 7, 30, 14, 30, 0, 0, time.UTC)) // 再開
+		work = work.Toggle(time.Date(2015, 7, 30, 18, 30, 0, 0, time.UTC)) // 停止
+		return work
+	}(),
+	func() Work {
+		work := New("作業04", 0)
+		work = work.Start(time.Date(2015, 7, 27, 10, 00, 0, 0, time.UTC))  // 開始
+		work = work.Toggle(time.Date(2015, 7, 27, 12, 00, 0, 0, time.UTC)) // 停止
+		work = work.Toggle(time.Date(2015, 7, 27, 14, 30, 0, 0, time.UTC)) // 再開
+		work = work.Toggle(time.Date(2015, 7, 27, 18, 30, 0, 0, time.UTC)) // 停止
+		return work
+	}(),
+	func() Work {
+		work := New("作業05", 0)
+		work = work.Start(time.Date(2015, 7, 28, 10, 00, 0, 0, time.UTC))  // 開始
+		work = work.Toggle(time.Date(2015, 7, 28, 12, 00, 0, 0, time.UTC)) // 停止
+		work = work.Toggle(time.Date(2015, 7, 28, 14, 30, 0, 0, time.UTC)) // 再開
+		work = work.Finish(time.Date(2015, 7, 28, 18, 30, 0, 0, time.UTC)) // 完了
+		return work
+	}(),
 }
