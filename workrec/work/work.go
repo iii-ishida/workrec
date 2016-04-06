4d4b7ec9f0777f526b6019456cbaca2d5eb59a17
iii-ishida <dev.j.ishida@gmail.com>
2016-04-06 10:56:29 +0900

    Work の Start,Toggle,Finish を実装

--- workrec/work/work.go
+++ workrec/work/work.go
@@ -14,6 +14,23 @@ const (
 	Finish
 )
 
+func (state State) String() string {
+	switch state {
+	case Unknown:
+		return "Unknown"
+	case Start:
+		return "Start"
+	case Pause:
+		return "Pause"
+	case Resume:
+		return "Resume"
+	case Finish:
+		return "Finish"
+	default:
+		return "Unknown"
+	}
+}
+
 type action struct {
 	State State
 	Time  time.Time
@@ -25,14 +42,63 @@ type Work struct {
 	GoalMinutes int
 }
 
+func New(title string, goalMinutes int) Work {
+	return Work{
+		Title:       title,
+		Actions:     []action{},
+		GoalMinutes: goalMinutes,
+	}
+}
+
 func (work Work) Start(time time.Time) Work {
+	if work.CurrentState() != Unknown {
+		return work
+	}
+
+	work.Actions = append(work.Actions, action{State: Start, Time: time})
 	return work
 }
 
 func (work Work) Toggle(time time.Time) Work {
+	nextState := work.nextState()
+	if nextState == Unknown {
+		return work
+	}
+
+	work.Actions = append(work.Actions, action{State: nextState, Time: time})
 	return work
 }
 
 func (work Work) Finish(time time.Time) Work {
+	if work.CurrentState() == Unknown {
+		return work
+	}
+
+	work.Actions = append(work.Actions, action{State: Finish, Time: time})
 	return work
+}
+
+func (work Work) CurrentState() State {
+	len := len(work.Actions)
+	if len == 0 {
+		return Unknown
+	}
+	return work.Actions[len-1].State
+}
+
+func (work Work) nextState() State {
+	switch work.CurrentState() {
+	case Unknown:
+		return Unknown
+	case Start:
+		return Pause
+	case Pause:
+		return Resume
+	case Resume:
+		return Pause
+	case Finish:
+		return Unknown
+	default:
+		return Unknown
+	}
 }
--- workrec/work/work_test.go
+++ workrec/work/work_test.go
@@ -2,13 +2,97 @@ package work
 
 import (
 	"testing"
+	"time"
 )
 
+func TestCurrentState(t *testing.T) {
+	work := New("TestCurrentState", 0)
+	want := Unknown
+	if work.CurrentState() != want {
+		t.Errorf("work.CurrentState() = %+v, want %+v", work.CurrentState(), want)
+	}
+
+	work = work.Start(time.Date(2015, 7, 29, 9, 30, 0, 0, time.UTC))
+	want = Start
+	if work.CurrentState() != want {
+		t.Errorf("work.CurrentState() = %+v, want %+v", work.CurrentState(), want)
+	}
+
+	work = work.Toggle(time.Date(2015, 7, 29, 12, 30, 0, 0, time.UTC))
+	want = Pause
+	if work.CurrentState() != want {
+		t.Errorf("work.CurrentState() = %+v, want %+v", work.CurrentState(), want)
+	}
+
+	work = work.Toggle(time.Date(2015, 7, 29, 13, 25, 0, 0, time.UTC))
+	want = Resume
+	if work.CurrentState() != want {
+		t.Errorf("work.CurrentState() = %+v, want %+v", work.CurrentState(), want)
+	}
+
+	work = work.Finish(time.Date(2015, 7, 29, 18, 30, 0, 0, time.UTC))
+	want = Finish
+	if work.CurrentState() != want {
+		t.Errorf("work.CurrentState() = %+v, want %+v", work.CurrentState(), want)
+	}
+}
+
 func TestStartWork(t *testing.T) {
+	original := New("TestStartWork", 0)
+
+	started := original.Start(time.Date(2015, 7, 29, 9, 30, 0, 0, time.UTC))
+	want := Start
+	if started.CurrentState() != want {
+		t.Errorf("started.CurrentState() = %+v, want %+v", started.CurrentState(), want)
+	}
+
+	want = Unknown
+	if original.CurrentState() != want {
+		t.Errorf("original.CurrentState() = %+v, want %+v", original.CurrentState(), want)
+	}
 }
 
 func TestToggleWork(t *testing.T) {
+	startTime := time.Date(2015, 7, 29, 9, 30, 0, 0, time.UTC)
+	pauseTime := time.Date(2015, 7, 29, 12, 30, 0, 0, time.UTC)
+	resumeTime := time.Date(2015, 7, 29, 13, 25, 0, 0, time.UTC)
+
+	original := New("TestToggleWork", 0)
+
+	paused := original.Start(startTime).Toggle(pauseTime)
+	want := Pause
+	if paused.CurrentState() != want {
+		t.Errorf("paused.CurrentState() = %+v, want %+v", paused.CurrentState(), want)
+	}
+
+	resumed := paused.Toggle(resumeTime)
+	want = Resume
+	if resumed.CurrentState() != want {
+		t.Errorf("resumed.CurrentState() = %+v, want %+v", resumed.CurrentState(), want)
+	}
+
+	want = Unknown
+	if original.CurrentState() != want {
+		t.Errorf("original.CurrentState() = %+v, want %+v", original.CurrentState(), want)
+	}
 }
 
 func TestFinishWork(t *testing.T) {
+	startTime := time.Date(2015, 7, 29, 9, 30, 0, 0, time.UTC)
+	pauseTime := time.Date(2015, 7, 29, 12, 30, 0, 0, time.UTC)
+	resumeTime := time.Date(2015, 7, 29, 13, 25, 0, 0, time.UTC)
+	finishTime := time.Date(2015, 7, 29, 18, 30, 0, 0, time.UTC)
+
+	original := New("TestFinishWork", 0)
+
+	finished := original.Start(startTime).Toggle(pauseTime).Toggle(resumeTime).Finish(finishTime)
+	want := Finish
+	if finished.CurrentState() != want {
+		t.Errorf("finished.CurrentState() = %+v, want %+v", finished.CurrentState(), want)
+	}
+
+	want = Unknown
+	if original.CurrentState() != want {
+		t.Errorf("original.CurrentState() = %+v, want %+v", original.CurrentState(), want)
+	}
 }
