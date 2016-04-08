b6f6fd73994e97ad1b6d4728255ad32121c6f9a1
iii-ishida <dev.j.ishida@gmail.com>
2016-04-08 15:26:12 +0900

    Work の CRUD 処理を実装

--- src/workrec/db/work_db.go
+++ src/workrec/db/work_db.go
@@ -1,14 +1,31 @@
 package db
 
 import (
+	"net/http"
 	"strconv"
 	"sync"
+
+	"golang.org/x/net/context"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/datastore"
+
+	"workrec/work"
 )
+
+const _Kind = "Works"
 
 var (
 	currentId     = 0
 	currentIdLock sync.RWMutex
 )
+
+type AppengineDB struct {
+	ctx context.Context
+}
+
+func NewAppEngineDB(r *http.Request) AppengineDB {
+	return AppengineDB{appengine.NewContext(r)}
+}
 
 func NextWorkID() string {
 	currentIdLock.Lock()
@@ -17,4 +34,25 @@ func NextWorkID() string {
 	currentId += 1
 
 	return strconv.Itoa(currentId)
+}
+
+func (db AppengineDB) GetAllWorks() (work.WorkList, error) {
+	query := datastore.NewQuery(_Kind)
+	workList := work.WorkList{}
+	_, err := query.GetAll(db.ctx, &workList)
+	return workList, err
+}
+
+func (db AppengineDB) SaveWork(wk work.Work) (work.Work, error) {
+	if wk.ID == "" {
+		wk.ID = NextWorkID()
+	}
+	key := datastore.NewKey(db.ctx, _Kind, wk.ID, 0, nil)
+	_, err := datastore.Put(db.ctx, key, &wk)
+	return wk, err
+}
+
+func (db AppengineDB) DeleteWork(wk work.Work) error {
+	key := datastore.NewKey(db.ctx, _Kind, wk.ID, 0, nil)
+	return datastore.Delete(db.ctx, key)
 }
--- src/workrec/db/work_db_test.go
+++ src/workrec/db/work_db_test.go
@@ -1,7 +1,13 @@
 package db
 
 import (
+	"strings"
 	"testing"
+
+	"google.golang.org/appengine/aetest"
+	"google.golang.org/appengine/datastore"
+
+	"workrec/work"
 )
 
 func TestNextWorkID(t *testing.T) {
@@ -35,3 +41,218 @@ func TestNextWorkID(t *testing.T) {
 		workIDs[workID] = struct{}{}
 	}
 }
+
+func TestCRUD(t *testing.T) {
+	inst, err := aetest.NewInstance(nil)
+	if err != nil {
+		t.Fatalf("Failed to create instance: %v", err)
+	}
+	defer inst.Close()
+
+	req, err := inst.NewRequest("GET", "/works", nil)
+	db := NewAppEngineDB(req)
+
+	// Save
+	for i, wk := range works {
+		savedWork, err := db.SaveWork(wk)
+		if err != nil {
+			t.Errorf("works[%d], SaveWork err = %+v, want nil", i, err)
+		} else if savedWork.ID == "" {
+			t.Errorf("savedWorks[%d].ID = \"\"", i)
+		} else if wk.ID != "" {
+			t.Errorf("work[%d].ID = %+v, want \"\"", i, wk.ID)
+		}
+		works[i].ID = savedWork.ID
+
+		tmp := work.Work{}
+		datastore.Get(db.ctx, datastore.NewKey(db.ctx, "Works", savedWork.ID, 0, nil), &tmp)
+	}
+
+	// GetAll
+	savedWorks, err := db.GetAllWorks()
+	if err != nil {
+		t.Errorf("GetAllWorks err = %+v, want nil", err)
+	}
+	if !savedWorks.Equal(works) {
+		t.Errorf("savedWorks.Equal(works) = false, want true")
+	}
+
+	// Save (Update)
+	const updateWorkIdx = 3
+	works[updateWorkIdx].Title = "UPDATE TITLE"
+	if _, err := db.SaveWork(works[updateWorkIdx]); err != nil {
+		t.Errorf("works[3], SaveWork(Update) err = %+v, want nil", err)
+	}
+	tmp := work.Work{}
+	datastore.Get(db.ctx, datastore.NewKey(db.ctx, "Works", works[updateWorkIdx].ID, 0, nil), &tmp)
+
+	updatedWorks, _ := db.GetAllWorks()
+	if !updatedWorks.Equal(works) {
+		t.Errorf("updatedWorks.Equal(works) = false, want true")
+	}
+	if updatedWorks.Equal(savedWorks) {
+		t.Errorf("updatedWorks.Equal(savedWorks) = true, want false")
+	}
+
+	// Delete
+	for i, wk := range works {
+		if err := db.DeleteWork(wk); err != nil {
+			t.Errorf("works[%d], DeleteWork err = %+v, want nil", i, err)
+		}
+		tmp := work.Work{}
+		datastore.Get(db.ctx, datastore.NewKey(db.ctx, "Works", wk.ID, 0, nil), &tmp)
+	}
+
+	if allWorks, _ := db.GetAllWorks(); len(allWorks) != 0 {
+		t.Errorf("len(GetAllWorks) = %d, want 0", len(allWorks))
+	}
+
+	if err := db.DeleteWork(work.Work{}); err == nil {
+		t.Errorf("DeleteWork(Work{}]) err nil, want not nil")
+	}
+}
+
+const workListJSONString = `
+[
+	{
+		"title": "作業01",
+		"actions": [
+			{
+				"state": 1,
+				"time": "2015-07-29T09:30:00+00:00"
+			},
+			{
+				"state": 2,
+				"time": "2015-07-29T12:30:00+00:00"
+			},
+			{
+				"state": 3,
+				"time": "2015-07-29T13:30:00+00:00"
+			},
+			{
+				"state": 2,
+				"time": "2015-07-29T18:30:00+00:00"
+			}
+		],
+		"goal_minutes": 0
+	},
+	{
+		"title": "作業02",
+		"actions": [
+			{
+				"state": 1,
+				"time": "2015-07-28T10:00:00+00:00"
+			},
+			{
+				"state": 2,
+				"time": "2015-07-28T12:00:00+00:00"
+			},
+			{
+				"state": 3,
+				"time": "2015-07-28T14:30:00+00:00"
+			},
+			{
+				"state": 2,
+				"time": "2015-07-28T19:30:00+00:00"
+			},
+			{
+				"state": 3,
+				"time": "2015-07-29T10:00:00+00:00"
+			},
+			{
+				"state": 2,
+				"time": "2015-07-29T12:00:00+00:00"
+			},
+			{
+				"state": 3,
+				"time": "2015-07-29T14:30:00+00:00"
+			},
+			{
+				"state": 2,
+				"time": "2015-07-30T01:00:00+00:00"
+			},
+			{
+				"state": 3,
+				"time": "2015-07-30T13:30:00+00:00"
+			},
+			{
+				"state": 2,
+				"time": "2015-07-30T15:30:00+00:00"
+			},
+			{
+				"state": 3,
+				"time": "2015-07-30T16:30:00+00:00"
+			}
+		],
+		"goal_minutes": 500
+	},
+	{
+		"title": "作業03",
+		"actions": [
+			{
+				"state": 1,
+				"time": "2015-07-30T10:00:00+00:00"
+			},
+			{
+				"state": 2,
+				"time": "2015-07-30T12:00:00+00:00"
+			},
+			{
+				"state": 3,
+				"time": "2015-07-30T14:30:00+00:00"
+			},
+			{
+				"state": 2,
+				"time": "2015-07-30T18:30:00+00:00"
+			}
+		],
+		"goal_minutes": 1000
+	},
+	{
+		"title": "作業04",
+		"actions": [
+			{
+				"state": 1,
+				"time": "2015-07-27T10:00:00+00:00"
+			},
+			{
+				"state": 2,
+				"time": "2015-07-27T12:00:00+00:00"
+			},
+			{
+				"state": 3,
+				"time": "2015-07-27T14:30:00+00:00"
+			},
+			{
+				"state": 2,
+				"time": "2015-07-27T18:30:00+00:00"
+			}
+		],
+		"goal_minutes": 750
+	},
+	{
+		"title": "作業05",
+		"actions": [
+			{
+				"state": 1,
+				"time": "2015-07-28T10:00:00+00:00"
+			},
+			{
+				"state": 2,
+				"time": "2015-07-28T12:00:00+00:00"
+			},
+			{
+				"state": 3,
+				"time": "2015-07-28T14:30:00+00:00"
+			},
+			{
+				"state": 4,
+				"time": "2015-07-28T18:30:00+00:00"
+			}
+		],
+		"goal_minutes": 0
+	}
+]
+`
+
+var works, _ = work.WorkListFromJSON(strings.NewReader(workListJSONString))
