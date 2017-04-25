2ffaab7ae3eeb4ed76243a659f9ed7a991118bcb
iii-ishida <dev.j.ishida@gmail.com>
2017-04-25 23:16:24 +0900

    modelを追加

--- libs/util/util.go
+++ libs/util/util.go
@@ -1,8 +1,17 @@
 package util
 
-import uuid "github.com/satori/go.uuid"
+import (
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+)
 
 // NewUUID returns a UUID String.
 func NewUUID() string {
 	return uuid.NewV4().String()
+}
+
+// Now returns the current time as a unix nanoseconds.
+func Now() int64 {
+	return time.Now().UTC().UnixNano()
 }
