c9384de48f2b080e6810e50cd6b27627a71d3e61
iii-ishida <dev.j.ishida@gmail.com>
2018-11-22 22:23:51 +0900

    server: test.sh 修正
    各パッケージを並列でテストしないように修正

--- server/scripts/test.sh
+++ server/scripts/test.sh
@@ -2,4 +2,4 @@
 
 BASEDIR=$(cd $(dirname $(dirname "$0")) && pwd)
 
-cd $BASEDIR && go mod verify && go vet ./... && golint -set_exit_status ./... && go test ./...
+cd $BASEDIR && go mod verify && go vet ./... && golint -set_exit_status ./... && go list ./... | xargs -L 1 go test
