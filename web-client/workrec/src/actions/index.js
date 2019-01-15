6d1c9a9bcba1c9f86fcf564159615e57c42e031e
iii-ishida <dev.j.ishida@gmail.com>
2019-01-15 18:57:09 +0900

    web-client: state 変更用の action を追加

--- web-client/workrec/src/actions/index.js
+++ web-client/workrec/src/actions/index.js
@@ -1,7 +1,12 @@
 import API from '../api'
 
-export const RECIEVE_WORKS = 'RECIEVE_WORKS'
-export const DELETE_WORK = 'DELETE_WORK'
+export const RECIEVE_WORKS      = 'RECIEVE_WORKS'
+export const START_WORK         = 'START_WORK'
+export const PAUSE_WORK         = 'PAUSE_WORK'
+export const RESUME_WORK        = 'RESUME_WORK'
+export const FINISH_WORK        = 'FINISH_WORK'
+export const CANCEL_FINISH_WORK = 'CANCEL_FINISH_WORK'
+export const DELETE_WORK        = 'DELETE_WORK'
 
 const recieveWorks = worklist => ({
   type: RECIEVE_WORKS,
@@ -22,6 +27,35 @@ export function addWork(title) {
   }
 }
 
+export function startWork(id, time) {
+  return dispatch => {
+    return API.startWork(id, time).then(() => dispatch(fetchWorks()))
+  }
+}
+
+export function pauseWork(id, time) {
+  return dispatch => {
+    return API.pauseWork(id, time).then(() => dispatch(fetchWorks()))
+  }
+}
+
+export function resumeWork(id, time) {
+  return dispatch => {
+    return API.resumeWork(id, time).then(() => dispatch(fetchWorks()))
+  }
+}
+
+export function finishWork(id, time) {
+  return dispatch => {
+    return API.finishWork(id, time).then(() => dispatch(fetchWorks()))
+  }
+}
+
+export function cancelFinishWork(id, time) {
+  return dispatch => {
+    return API.cancelFinishWork(id, time).then(() => dispatch(fetchWorks()))
+  }
+}
 
 export function deleteWork(id) {
   return dispatch => {
