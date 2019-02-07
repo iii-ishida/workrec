1188aa6833e210a76ae2af0d129add7d7778d6d5
iii-ishida <dev.j.ishida@gmail.com>
2019-02-07 19:19:35 +0900

    web-client: DevAPI を削除

--- web-client/workrec/src/api/index.js
+++ web-client/workrec/src/api/index.js
@@ -1,8 +1,5 @@
-import API from './api.prod.js'
-import DevAPI from './api.dev.js'
-
+import API from './api.js'
 import * as WorkState from './work_state.js'
 
 export default API
-export { DevAPI }
 export { WorkState }
