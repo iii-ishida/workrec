daac73cf79d5208e5da1e14a8058a227f9b9d459
iii-ishida <dev.j.ishida@gmail.com>
2019-06-25 21:15:33 +0900

    web-client: ホスティング方法を GAE から Firebase Hosting に変更

--- web-client/scripts/build.sh
+++ web-client/scripts/build.sh
@@ -2,8 +2,8 @@
 
 BASEDIR=$(cd $(dirname $(dirname "$0")) && pwd)
 
-rm -rf $BASEDIR/server/dist &&
+rm -rf $BASEDIR/dist &&
 cd $BASEDIR/app &&
 echo $FIREBASE_CONFIG | base64 --decode > $BASEDIR/app/src/firebase-config.js &&
 yarn install && REACT_APP_API_ORIGIN=${API_ORIGIN} yarn build &&
-cp -r $BASEDIR/app/build $BASEDIR/server/dist
+cp -r $BASEDIR/app/build $BASEDIR/dist
