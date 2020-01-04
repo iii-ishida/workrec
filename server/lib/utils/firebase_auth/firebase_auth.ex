864a11b0a2f1d0a6287a6bb39c57e440c10e1b52
iii-ishida <dev.j.ishida@gmail.com>
2020-01-04 13:10:28 +0900

    feat(server): 認証時に使用する certs をキャッシュする

--- server/lib/utils/firebase_auth/firebase_auth.ex
+++ server/lib/utils/firebase_auth/firebase_auth.ex
@@ -3,6 +3,8 @@ defmodule Utils.FirebaseAuth do
   FirebaseAuth
   """
 
+  alias Utils.FirebaseAuth.GoogleCerts
+
   @doc """
   Verify id_token.
 
@@ -51,14 +53,54 @@ defmodule Utils.FirebaseAuth do
   end
 end
 
-defmodule GoogleCerts do
+defmodule Utils.FirebaseAuth.GoogleCerts do
   @moduledoc false
 
   @certs_url "https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com"
 
   def fetch do
-    with {:ok, res} <- HTTPoison.get(@certs_url) do
-      Jason.decode(res.body)
+    case get_cache() do
+      {:ok, _} = cache -> cache
+      _ -> do_fetch()
+    end
+  end
+
+  defp do_fetch do
+    with {:ok, res} <- HTTPoison.get(@certs_url),
+         {:ok, certs} <- Jason.decode(res.body) do
+      expire_at = calc_expire_at(res.headers)
+      save_cache(%{certs: certs, expire_at: expire_at})
+
+      {:ok, certs}
+    end
+  end
+
+  defp get_cache do
+    if :erlang.whereis(__MODULE__) != :undefined do
+      Agent.get(__MODULE__, & &1) |> do_get_cache()
+    end
+  end
+
+  defp do_get_cache(%{certs: certs, expire_at: expire_at}) do
+    if expire_at > DateTime.utc_now(), do: {:ok, certs}
+  end
+
+  defp do_get_cache(_), do: nil
+
+  defp save_cache(certs) do
+    if :erlang.whereis(__MODULE__) == :undefined do
+      Agent.start_link(fn -> [] end, name: __MODULE__)
+    end
+
+    Agent.update(__MODULE__, fn _ -> certs end)
+  end
+
+  defp calc_expire_at(headers) do
+    header_map = headers |> Enum.into(%{})
+
+    with %{"max_age" => max_age} <- Regex.named_captures(~r/max-age=(?<max_age>[0-9]+)/, header_map["Cache-Control"] || "") do
+      max_age = String.to_integer(max_age)
+      DateTime.utc_now() |> DateTime.add(max_age)
     end
   end
 end
