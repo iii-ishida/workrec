231ab3fc31454068c54add65525c3b2c93fc1de9
iii-ishida <dev.j.ishida@gmail.com>
2020-01-09 18:43:50 +0900

    refactor(server): 微修正

--- server/lib/workrec/repositories/cloud_datastore.ex
+++ server/lib/workrec/repositories/cloud_datastore.ex
@@ -12,26 +12,20 @@ defmodule Workrec.Repositories.CloudDatastore do
   alias Workrec.TaskList
   alias Workrec.TaskListItem
 
-  defstruct [:store]
+  defstruct [:connection]
 
-  @type commit_response :: GoogleApi.Datastore.V1.Model.CommitResponse.t() | nil
-  @type t :: %__MODULE__{store: Connection.t()}
-
-  @spec new :: {:ok, t} | {:error, term}
   def new do
     with {:ok, connection} <- Connection.new() do
-      {:ok, %__MODULE__{store: connection}}
+      {:ok, %__MODULE__{connection: connection}}
     end
   end
 
-  @spec run_in_transaction(t, (t -> term)) :: {:ok, term} | {:error, term}
-  def run_in_transaction(%{store: store}, fun) do
-    Datastore.run_in_transaction(store, fn tx ->
-      fun.(%__MODULE__{store: tx})
+  def run_in_transaction(%{connection: connection}, fun) do
+    Datastore.run_in_transaction(connection, fn tx ->
+      fun.(%__MODULE__{connection: tx})
     end)
   end
 
-  @spec run_in_transaction((t -> term)) :: {:ok, term} | {:error, term}
   def run_in_transaction(fun) do
     case new() do
       {:ok, repo} -> run_in_transaction(repo, fun)
@@ -39,9 +33,8 @@ defmodule Workrec.Repositories.CloudDatastore do
     end
   end
 
-  @spec find(t, module, String.t()) :: {:ok, term} | {:error, term}
-  def find(%{store: store}, module, id) do
-    with {:ok, found} <- Datastore.find(store, Key.new(module.kind_name(), id)) do
+  def find(%{connection: connection}, module, id) do
+    with {:ok, found} <- Datastore.find(connection, Key.new(module.kind_name(), id)) do
       case found do
         nil -> {:ok, nil}
         found -> {:ok, module.from_entity(found)}
@@ -49,21 +42,18 @@ defmodule Workrec.Repositories.CloudDatastore do
     end
   end
 
-  @spec find!(t, module, String.t()) :: term | no_return
-  def find!(store, module, id) do
-    case find(store, module, id) do
+  def find!(connection, module, id) do
+    case find(connection, module, id) do
       {:ok, found} -> found
       {:error, reason} -> raise reason
     end
   end
 
-  @spec insert(t, list(struct) | struct) :: {:ok, commit_response} | {:error, term}
-  def insert(%{store: store}, models) do
+  def insert(%{connection: connection}, models) do
     entity = Decoder.to_entity(models)
-    Datastore.insert(store, entity)
+    Datastore.insert(connection, entity)
   end
 
-  @spec insert!(t, list(struct) | struct) :: commit_response | no_return
   def insert!(repo, models) do
     case insert(repo, models) do
       {:ok, result} -> result
@@ -71,13 +61,11 @@ defmodule Workrec.Repositories.CloudDatastore do
     end
   end
 
-  @spec upsert(t, list(struct) | struct) :: {:ok, commit_response} | {:error, term}
-  def upsert(%{store: store}, models) do
+  def upsert(%{connection: connection}, models) do
     entity = Decoder.to_entity(models)
-    Datastore.upsert(store, entity)
+    Datastore.upsert(connection, entity)
   end
 
-  @spec upsert!(t, list(struct) | struct) :: commit_response | no_return
   def upsert!(repo, models) do
     case upsert(repo, models) do
       {:ok, result} -> result
@@ -85,13 +73,11 @@ defmodule Workrec.Repositories.CloudDatastore do
     end
   end
 
-  @spec update(t, struct) :: {:ok, commit_response} | {:error, term}
-  def update(%{store: store}, models) do
+  def update(%{connection: connection}, models) do
     entity = Decoder.to_entity(models)
-    Datastore.update(store, entity)
+    Datastore.update(connection, entity)
   end
 
-  @spec update!(t, list(struct) | struct) :: commit_response | no_return
   def update!(repo, models) do
     case update(repo, models) do
       {:ok, result} -> result
@@ -99,24 +85,23 @@ defmodule Workrec.Repositories.CloudDatastore do
     end
   end
 
-  @spec delete(t, list(struct) | struct) :: {:ok, commit_response} | {:error, term}
-  def delete(%{store: store}, models) when is_list(models) do
+  def delete(%{connection: connection}, models) when is_list(models) do
     keys = Enum.map(models, &Key.new(&1.__struct__.kind_name, &1.id))
-    Datastore.delete(store, keys)
+    Datastore.delete(connection, keys)
   end
 
-  def delete(%{store: store}, model) do
-    delete(store, [model])
+  def delete(%{connection: connection}, model) do
+    delete(connection, [model])
   end
 
-  @spec delete!(t, list(struct) | struct) :: :ok | no_return
-  def delete!(%{store: store}, models) do
-    keys = Enum.map(models, &Key.new(&1.__struct__.kind_name, &1.id))
-    Datastore.delete!(store, keys)
+  def delete!(repo, models) do
+    case delete(repo, models) do
+      :ok -> :ok
+      {:error, reason} -> raise reason
+    end
   end
 
-  @spec list_events(t, String.t(), DateTime.t(), non_neg_integer(), String.t()) :: {:ok, list(Event.t())} | {:error, term}
-  def list_events(%{store: store}, user_id, created_at, limit \\ 100, page_token \\ "") do
+  def list_events(%{connection: connection}, user_id, created_at, limit \\ 100, page_token \\ "") do
     import DsWrapper.Query
 
     query =
@@ -128,7 +113,7 @@ defmodule Workrec.Repositories.CloudDatastore do
       |> limit(limit)
       |> start(page_token)
 
-    with {:ok, result} <- Datastore.run_query(store, query) do
+    with {:ok, result} <- Datastore.run_query(connection, query) do
       {:ok, Enum.map(result.entities, &Event.from_entity/1)}
     end
   end
@@ -140,8 +125,7 @@ defmodule Workrec.Repositories.CloudDatastore do
     end
   end
 
-  @spec find_last_event(t, String.t(), String.t()) :: {:ok, term} | {:error, term}
-  def find_last_event(%{store: store}, user_id, task_id) do
+  def find_last_event(%{connection: connection}, user_id, task_id) do
     import DsWrapper.Query
 
     query =
@@ -153,14 +137,13 @@ defmodule Workrec.Repositories.CloudDatastore do
       |> order("created_at", :desc)
       |> limit(1)
 
-    case Datastore.run_query(store, query) do
+    case Datastore.run_query(connection, query) do
       {:ok, %{entities: []}} -> {:ok, nil}
       {:ok, %{entities: [entity | _]}} -> {:ok, Event.from_entity(entity)}
       error -> error
     end
   end
 
-  @spec find_last_event!(t, String.t(), String.t()) :: term | no_return
   def find_last_event!(repo, user_id, task_id) do
     case find_last_event(repo, user_id, task_id) do
       {:ok, result} -> result
@@ -168,8 +151,7 @@ defmodule Workrec.Repositories.CloudDatastore do
     end
   end
 
-  @spec list_tasks(t, String.t(), non_neg_integer, String.t()) :: {:ok, TaskList.t()} | {:error, term}
-  def list_tasks(%{store: store}, user_id, limit \\ 100, page_token \\ "") do
+  def list_tasks(%{connection: connection}, user_id, limit \\ 100, page_token \\ "") do
     import DsWrapper.Query
 
     query =
@@ -180,7 +162,7 @@ defmodule Workrec.Repositories.CloudDatastore do
       |> limit(limit)
       |> start(page_token)
 
-    with {:ok, result} <- Datastore.run_query(store, query) do
+    with {:ok, result} <- Datastore.run_query(connection, query) do
       {:ok, TaskList.from_entity(result)}
     end
   end
