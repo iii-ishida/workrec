dae945c464fbb2919545e0ecdd7ba93000afc371
iii-ishida <dev.j.ishida@gmail.com>
2020-01-29 21:54:02 +0900

    feat(web-client): ルーターを追加

--- web-client/app/src/components/App.tsx
+++ web-client/app/src/components/App.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import TaskList from 'src/containers/TaskList'
 import AddTask from 'src/containers/AddTask'
 import Login from 'src/components/Login'
 
 const App: React.FC = () => (
-  <div>
-    <TaskList />
-    <AddTask />
-    <Login />
-  </div>
+  <Router>
+    <Switch>
+      <Route exact path='/'>
+        <TaskList />
+        <AddTask />
+      </Route>
+      <Route exact path='/signin'>
+        <Login />
+      </Route>
+    </Switch>
+  </Router>
 )
 
 export default App
