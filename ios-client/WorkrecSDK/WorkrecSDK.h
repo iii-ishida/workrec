20f80828fab0736e5bbae03b4fa6c693e305baed
iii-ishida <dev.j.ishida@gmail.com>
2020-02-17 22:22:58 +0900

    chore(ios-client): WorkrecSDK を追加

---
+++ ios-client/WorkrecSDK/WorkrecSDK.h
@@ -0,0 +1,18 @@
+//
+//  WorkrecSDK.h
+//  WorkrecSDK
+//
+//  Created by ishida on 2020/02/17.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WorkrecSDK.
+FOUNDATION_EXPORT double WorkrecSDKVersionNumber;
+
+//! Project version string for WorkrecSDK.
+FOUNDATION_EXPORT const unsigned char WorkrecSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WorkrecSDK/PublicHeader.h>
+
+
