e68ac060395531a07f9829561e63ff1324a1ef1d
iii-ishida <dev.j.ishida@gmail.com>
2020-08-20 21:49:55 +0900

    refactor(web-client): Router を index.tsx に移動

--- web-client/src/components/App.tsx
+++ web-client/src/components/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import PrivateRoute from 'src/containers/PrivateRoute'
 import TaskListPage from 'src/containers/TaskListPage'
 import Login from 'src/containers/Login'
@@ -10,16 +10,14 @@ type Props = {
 }
 const App: React.FC<Props> = ({ initialized }: Props) =>
   initialized ? (
-    <Router>
-      <Switch>
-        <PrivateRoute exact path="/">
-          <TaskListPage />
-        </PrivateRoute>
-        <Route path="/login">
-          <Login />
-        </Route>
-      </Switch>
-    </Router>
+    <Switch>
+      <PrivateRoute exact path="/">
+        <TaskListPage />
+      </PrivateRoute>
+      <Route path="/login">
+        <Login />
+      </Route>
+    </Switch>
   ) : (
     <Loading />
   )
--- web-client/src/index.tsx
+++ web-client/src/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './containers/App'
 import { store } from 'src/redux'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 )
