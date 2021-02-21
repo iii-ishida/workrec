d3489c6e0459c4e6483f0eaa65f65191b446012c
iii-ishida <dev.j.ishida@gmail.com>
2021-02-21 14:21:58 +0900

    feat(flutter-client): flutter-client を追加

---
+++ flutter-client/android/app/src/main/kotlin/com/example/workrec/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.workrec
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter-client/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Workrec',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Workrec'),
+      ),
+      body: Center(
+        child: Text(
+          '',
+          style: Theme.of(context).textTheme.headline4,
+        ),
+      ),
+    );
+  }
+}
