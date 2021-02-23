b4d9a31fd5bf8aa4dbd69caa01321008532743a1
iii-ishida <dev.j.ishida@gmail.com>
2021-02-23 19:38:22 +0900

    feat(flutter-client): Drawer を追加

--- flutter-client/lib/widgets/home.dart
+++ flutter-client/lib/widgets/home.dart
@@ -8,11 +8,38 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Workrec'),
       ),
+      drawer: _Drawer(),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () => Auth().signOut(),
-          child: Text('ログアウト'),
-        ),
+        child: Text('Hello'),
+      ),
+    );
+  }
+}
+
+class _Drawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          Container(
+            height: kToolbarHeight + MediaQuery.of(context).padding.top,
+            child: DrawerHeader(
+              child: Text('Workrec', style: TextStyle(color: Colors.white)),
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+          ),
+          ListTile(
+            title: Text('ログアウト'),
+            onTap: () {
+              Auth().signOut();
+              Navigator.pop(context);
+            },
+          ),
+        ],
       ),
     );
   }
