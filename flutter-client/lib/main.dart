3b55a4f61061a872246095fea7b98715ee092e28
iii-ishida <dev.j.ishida@gmail.com>
2021-06-30 17:34:35 +0900

    feat(flutter-client): debug banner を非表示に設定

--- flutter-client/lib/main.dart
+++ flutter-client/lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      debugShowCheckedModeBanner: false,
       home: AuthProvider(
         repo: authRepo,
         builder: ((userId) => userId.isEmpty
