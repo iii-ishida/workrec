57ad1c425e4279059873777e2e1e047bf7266899
iii-ishida <dev.j.ishida@gmail.com>
2021-09-19 20:55:06 +0900

    chore(server): 不要な設定を削除

--- server/mix.exs
+++ server/mix.exs
@@ -7,9 +7,9 @@ defmodule Workrec.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
-      deps: deps(),
+      deps: deps()
     ]
   end
 
