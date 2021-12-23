2e91633fcba893ca1e6865c18e6ce9ca89dd4948
iii-ishida <dev.j.ishida@gmail.com>
2021-12-23 21:35:29 +0900

    feat(flutter-client): 設定画面を追加

---
+++ flutter-client/lib/widgets/settings.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:workrec_app/auth_client/auth_client.dart';
+
+class Settings extends StatelessWidget {
+  final SettingsViewModel _viewModel;
+  Settings({Key? key, AuthClient? authClient})
+      : _viewModel = SettingsViewModel(authClient ?? AuthClient()),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('設定'), elevation: 0),
+      body: ListView(children: [
+        ListTile(title: const Text('ログアウト'), onTap: _viewModel.signOut),
+        const Divider(height: 1, color: Color(0xFFA5A5A5)),
+      ]),
+    );
+  }
+}
+
+class SettingsViewModel {
+  AuthClient authClient;
+  SettingsViewModel(this.authClient);
+
+  Future<void> signOut() async {
+    await authClient.signOut();
+  }
+}
