329f85bf77e90fa3ced005478d0b4b59cf0b4c4b
iii-ishida <594841+iii-ishida@users.noreply.github.com>
2022-07-17 18:51:30 +0900

    feat(flutter-client): debug banner を非表示に設定

--- flutter-client/lib/main.dart
+++ flutter-client/lib/main.dart
@@ -12,6 +12,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Workrec',
       theme: ThemeData(
         primarySwatch: Colors.blue,
