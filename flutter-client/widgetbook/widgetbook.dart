1538b1fe78105277853c3330ceee0a19f5e6d1ad
iii-ishida <594841+iii-ishida@users.noreply.github.com>
2022-07-17 18:50:08 +0900

    chore(flutter-client): widgetbook を追加

---
+++ flutter-client/widgetbook/widgetbook.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:widgetbook/widgetbook.dart';
+
+import 'package:workrec/widgets/home.dart';
+
+class HotReload extends StatelessWidget {
+  const HotReload({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Widgetbook(
+      appInfo: AppInfo(name: 'Workrec App'),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('ja', 'JP'),
+      ],
+      themes: [
+        WidgetbookTheme(
+          name: 'Light',
+          data: ThemeData.light(),
+        ),
+        WidgetbookTheme(
+          name: 'Dark',
+          data: ThemeData.dark(),
+        ),
+      ],
+      devices: const [
+        Apple.iPhoneSE2020,
+        Apple.iPhone13Mini,
+        Apple.iPhone13Pro,
+        Apple.iPhone13ProMax,
+      ],
+      categories: [
+        WidgetbookCategory(
+          name: 'pages',
+          folders: [
+            WidgetbookFolder(
+              name: 'Home',
+              widgets: [
+                WidgetbookComponent(
+                  name: 'Home',
+                  useCases: [
+                    WidgetbookUseCase(
+                      name: 'Dafault',
+                      builder: (context) => const Home(),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
