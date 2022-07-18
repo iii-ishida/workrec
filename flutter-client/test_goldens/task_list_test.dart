a53603d43fcd3c2bb9bf27c032fa9943092a5d96
iii-ishida <594841+iii-ishida@users.noreply.github.com>
2022-07-18 17:27:07 +0900

    feat(flutter-client): スクロール時のゴールデンテストを追加

--- flutter-client/test_goldens/task_list_test.dart
+++ flutter-client/test_goldens/task_list_test.dart
@@ -4,16 +4,37 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:workrec/widgets/task_list.dart';
 
 void main() {
-  testWidgets('Golden test', (WidgetTester tester) async {
-    await tester.pumpWidget(const MaterialApp(
-      title: 'Workrec',
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(body: TaskListPage()),
-    ));
+  group('TaskListScreen', () {
+    testWidgets('初期表示', (tester) async {
+      await _pumpTaskList(tester);
 
-    await tester.pump(Duration.zero);
+      await tester.pump(Duration.zero);
 
-    await expectLater(
-        find.byType(TaskListPage), matchesGoldenFile('task_list.png'));
+      await expectLater(
+        find.byType(TaskListScreen),
+        matchesGoldenFile('task_list.png'),
+      );
+    });
+
+    testWidgets('スクロール時', (tester) async {
+      await _pumpTaskList(tester);
+
+      final gesture = await tester.startGesture(const Offset(0, 500));
+      await gesture.moveTo(Offset.zero);
+      await tester.pumpAndSettle();
+
+      await expectLater(
+        find.byType(TaskListScreen),
+        matchesGoldenFile('task_list_scrolled.png'),
+      );
+    });
   });
+}
+
+Future<void> _pumpTaskList(WidgetTester tester) async {
+  await tester.pumpWidget(const MaterialApp(
+    title: 'Workrec',
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(body: TaskListPage()),
+  ));
 }
