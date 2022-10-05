addb2b22c9dd64eda0c318436a35ff58b233afa3
ishida <594841+iii-ishida@users.noreply.github.com>
2022-10-05 17:50:32 +0900

    chore(ios-client): プロジェクトを作り直し

---
+++ ios-client/Workrec/WorkrecApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkrecApp.swift
+//  Workrec
+//
+//  Created by ishida on 2022/10/05.
+//
+
+import SwiftUI
+
+@main
+struct WorkrecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
