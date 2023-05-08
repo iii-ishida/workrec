4ed0dc2e19f70332ee0ce72d3d06a0f7bfb9cce6
ishida <594841+iii-ishida@users.noreply.github.com>
2023-05-08 21:18:16 +0900

    chore: cypress のヘルパーを切り出し

--- cypress.config.js
+++ cypress.config.js
@@ -1,100 +1,9 @@
-module.exports = {
+import { defineConfig } from 'cypress'
+import { signInWithEmail, startTask, stopTask } from './cypress/helper'
+
+export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      const signInWithEmail = async (email) => {
-        const url =
-          config.env.firebaseAuthOrigin +
-          '/identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=fake-api-key'
-
-        return await fetch(url, {
-          method: 'POST',
-          body: JSON.stringify({
-            email,
-            password: 'PASSWORD',
-            returnSecureToken: true,
-          }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        })
-          .then((res) => {
-            return res.json()
-          })
-          .then((res) => {
-            return res.idToken
-          })
-      }
-
-      const startTask = async ({ idToken, taskId, timestamp }) => {
-        const url = config.env.apiOrigin + '/graphql'
-
-        const query = `
-          mutation Mutation($taskId: String!, $timestamp: DateTime!) {
-            startWorkOnTask(
-              taskId: $taskId,
-              timestamp: $timestamp
-           ) {
-              id
-              state
-              title
-              totalWorkingTime
-              lastWork {
-                id
-                startTime
-                endTime
-                workingTime
-              }
-            }
-          }
-        `
-
-        const variables = { taskId, timestamp }
-
-        return await fetch(url, {
-          method: 'POST',
-          body: JSON.stringify({ query, variables }),
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: 'Bearer ' + idToken,
-          },
-        }).then((res) => res.json())
-      }
-
-      const stopTask = async ({ idToken, taskId, timestamp }) => {
-        const url = config.env.apiOrigin + '/graphql'
-
-        const query = `
-          mutation Mutation($taskId: String!, $timestamp: DateTime!) {
-            stopWorkOnTask(
-              taskId: $taskId,
-              timestamp: $timestamp
-            ) {
-              id
-              state
-              title
-              totalWorkingTime
-              lastWork {
-                id
-                startTime
-                endTime
-                workingTime
-              }
-            }
-          }
-        `
-
-        const variables = { taskId, timestamp }
-
-        return await fetch(url, {
-          method: 'POST',
-          body: JSON.stringify({ query, variables }),
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: 'Bearer ' + idToken,
-          },
-        }).then((res) => res.json())
-      }
-
       on('task', {
         async createUser(email) {
           const url =
@@ -117,7 +26,7 @@ module.exports = {
         },
 
         async createTasks({ email, count }) {
-          const idToken = await signInWithEmail(email)
+          const idToken = await signInWithEmail(config, email)
 
           const query = `
             mutation Mutation($title: String!) {
@@ -151,19 +60,19 @@ module.exports = {
         },
 
         async startTask(arg) {
-          return startTask(arg)
+          return startTask(config, arg)
         },
         async toggleTask({ idToken, taskId, timestamp, count }) {
           const date = new Date(timestamp)
 
           for (let i = 0; i < count; i++) {
-            await startTask({
+            await startTask(config, {
               idToken,
               taskId,
               timestamp: new Date(date.getTime() + i * 2 * 1000 * 60),
             })
 
-            await stopTask({
+            await stopTask(config, {
               idToken,
               taskId,
               timestamp: new Date(date.getTime() + (i * 2 + 1) * 1000 * 60),
@@ -183,4 +92,4 @@ module.exports = {
     apiOrigin: 'http://localhost:8080',
     firebaseAuthOrigin: 'http://localhost:9099',
   },
-}
+})
---
+++ cypress/helper.js
@@ -0,0 +1,93 @@
+export const signInWithEmail = async (config, email) => {
+  const url =
+    config.env.firebaseAuthOrigin +
+    '/identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=fake-api-key'
+
+  return await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({
+      email,
+      password: 'PASSWORD',
+      returnSecureToken: true,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((res) => {
+      return res.json()
+    })
+    .then((res) => {
+      return res.idToken
+    })
+}
+
+export const startTask = async (config, { idToken, taskId, timestamp }) => {
+  const url = config.env.apiOrigin + '/graphql'
+
+  const query = `
+          mutation Mutation($taskId: String!, $timestamp: DateTime!) {
+            startWorkOnTask(
+              taskId: $taskId,
+              timestamp: $timestamp
+           ) {
+              id
+              state
+              title
+              totalWorkingTime
+              lastWork {
+                id
+                startTime
+                endTime
+                workingTime
+              }
+            }
+          }
+        `
+
+  const variables = { taskId, timestamp }
+
+  return await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({ query, variables }),
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + idToken,
+    },
+  }).then((res) => res.json())
+}
+
+export const stopTask = async (config, { idToken, taskId, timestamp }) => {
+  const url = config.env.apiOrigin + '/graphql'
+
+  const query = `
+          mutation Mutation($taskId: String!, $timestamp: DateTime!) {
+            stopWorkOnTask(
+              taskId: $taskId,
+              timestamp: $timestamp
+            ) {
+              id
+              state
+              title
+              totalWorkingTime
+              lastWork {
+                id
+                startTime
+                endTime
+                workingTime
+              }
+            }
+          }
+        `
+
+  const variables = { taskId, timestamp }
+
+  return await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({ query, variables }),
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + idToken,
+    },
+  }).then((res) => res.json())
+}
