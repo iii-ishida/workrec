c74ba25ee22a0869e2a50ae281af94358ffe98d7
ishida <594841+iii-ishida@users.noreply.github.com>
2023-08-19 16:59:06 +0900

    feat(web-client): singin画面のレイアウトを変更

--- web-client/app/route-containers/sign-in/sign-in.component.tsx
+++ web-client/app/route-containers/sign-in/sign-in.component.tsx
@@ -1,17 +1,9 @@
-import { V2_MetaFunction, useLoaderData, useSubmit } from '@remix-run/react'
+import { useLoaderData, useSubmit } from '@remix-run/react'
 import { ActionArgs, LoaderArgs, json, redirect } from '@remix-run/node'
 import { useCallback } from 'react'
 import { commitSession, getSession } from '~/services/session.server'
-import {
-  checkSessionCookie,
-  createSessionCookie,
-  getRestConfig,
-} from '~/services/auth.server'
+import { checkSessionCookie, createSessionCookie, getRestConfig } from '~/services/auth.server'
 import { signInWithEmailAndPassword } from '~/services/auth-client'
-
-export const meta: V2_MetaFunction = () => {
-  return [{ title: 'Sign In' }]
-}
 
 export async function loader({ request }: LoaderArgs) {
   const session = await getSession(request.headers.get('cookie'))
@@ -85,15 +77,39 @@ export default function Component() {
   )
 
   return (
-    <form method="post" onSubmit={handleSubmit}>
-      <input type="email" name="email" autoComplete="username" required />
-      <input
-        type="password"
-        name="password"
-        autoComplete="current-password"
-        required
-      />
-      <button>Sign In</button>
-    </form>
+    <div className="container m-10 mx-auto rounded-md border-2 border-gray-200 bg-gray-100 p-10">
+      <h1 className="text-center text-subhead font-semibold">Sign In</h1>
+      <form
+        className="flex flex-col gap-8 p-10"
+        method="post"
+        onSubmit={handleSubmit}
+      >
+        <div className="flex flex-col">
+          <label className="text-sm" htmlFor="email">
+            Email
+          </label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            autoComplete="username"
+            required
+          />
+        </div>
+        <div className="flex flex-col">
+          <label className="text-sm" htmlFor="password">
+            Password
+          </label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            autoComplete="current-password"
+            required
+          />
+        </div>
+        <button className="btn-rounded btn-primary self-end">Sign In</button>
+      </form>
+    </div>
   )
 }
--- web-client/app/routes/sign-in.tsx
+++ web-client/app/routes/sign-in.tsx
@@ -1,5 +1,10 @@
+import { V2_MetaFunction } from '@remix-run/react'
 export {
   default,
   action,
   loader,
 } from '~/route-containers/sign-in/sign-in.component'
+
+export const meta: V2_MetaFunction = () => {
+  return [{ title: 'Sign In' }]
+}
